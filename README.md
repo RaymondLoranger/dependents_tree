bfe0077ac5d0475ffe7d6be57fda442314fb80b0
RaymondLoranger <Raymond.Loranger@bell.net>
2019-01-09 19:17:32 -0500

    0.1.0

---
+++ README.md
@@ -0,0 +1,21 @@
+# Dependents.Tree
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `dependents_tree` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:dependents_tree, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/dependents_tree](https://hexdocs.pm/dependents_tree).
+
---
+++ mix.exs
@@ -0,0 +1,42 @@
+defmodule Dependents.Tree.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :dependents_tree,
+      version: "0.1.0",
+      elixir: "~> 1.7",
+      start_permanent: Mix.env() == :prod,
+      escript: escript(),
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:mix_tasks,
+       github: "RaymondLoranger/mix_tasks", only: :dev, runtime: false},
+      {:persist_config, "~> 0.1"},
+      {:io_ansi_table, "~> 0.4"},
+      {:earmark, "~> 1.0", only: :dev},
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false},
+      {:dialyxir, "~> 0.5", only: :dev, runtime: false}
+    ]
+  end
+
+  defp escript do
+    [
+      main_module: Dependents.Tree.CLI,
+      # :deps would collide with folder 'deps'
+      name: :deps_tree
+    ]
+  end
+end
