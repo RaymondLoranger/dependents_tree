84ed50d144c0eee4e61d5157b610fe86f0fbd329
RaymondLoranger <Raymond.Loranger@bell.net>
2023-02-12 06:00:06 -0500

    0.1.31

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Dependents.Tree.MixProject do
   def project do
     [
       app: :dependents_tree,
-      version: "0.1.30",
+      version: "0.1.31",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       name: "Dependents Tree",
